6f9c102e8853e8a99f4ecd7b5b1f34badf653be7
Zenxxanlol2 <zenxxanlawit@gmail.com>
2025-09-20 23:57:32 +0800

    Update whitelist.js

--- pages/api/whitelist.js
+++ pages/api/whitelist.js
@@ -1,1 +1,11 @@
+import fs from "fs";
+import path from "path";
 
+export default function handler(req, res) {
+  const whitelistPath = path.join(process.cwd(), "whitelist.json");
+  if (!fs.existsSync(whitelistPath)) {
+    return res.status(200).json([]);
+  }
+  const whitelist = JSON.parse(fs.readFileSync(whitelistPath));
+  res.status(200).json(whitelist);
+}
