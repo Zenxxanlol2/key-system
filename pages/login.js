72e921f379bd37a08aa73a5b23a5d2e93c3e31d1
Zenxxanlol2 <zenxxanlawit@gmail.com>
2025-09-21 00:22:53 +0800

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -3,15 +3,21 @@ import { useEffect } from "react";
 export default function Login() {
   useEffect(() => {
     const clientId = process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID;
+
+    if (!clientId) {
+      console.error("❌ Missing NEXT_PUBLIC_DISCORD_CLIENT_ID in env vars!");
+      return;
+    }
+
     const redirectUri = encodeURIComponent(
       "site-4yp5.vercel.app"
     );
+
     const scope = "identify";
 
-    
     const loginUrl = `https://discord.com/oauth2/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${scope}`;
 
-
+    // Redirect to Discord login
     window.location.href = loginUrl;
   }, []);
 
